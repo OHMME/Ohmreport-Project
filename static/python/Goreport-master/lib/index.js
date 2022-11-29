6d6ad06510c9dfb20b144974f511a196680acb30
OHMME <56262530+OHMME@users.noreply.github.com>
2022-11-30 05:34:54 +0700

    Update index.js

--- static/python/Goreport-master/lib/index.js
+++ static/python/Goreport-master/lib/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
-const { spawn } = require('child_process')
+const { spawn } = require('child_process');
+const { REFUSED } = require('dns');
 const app = express();
+var router = express.Router();
 
+var idx = ""
 
 
 app.get('/report', (req, res) => {
@@ -10,8 +13,14 @@ app.get('/report', (req, res) => {
     const process = spawn('python3', [`./goreport.py --id ${id} --format word`], { shell: true })
     
     process.stdout.on('data', function (data) {
+
         if(data.toString()) {
             res.send('generate report successfuly.')
+            var Result_output = ""
+            Result_output += data.toString()
+            idx = Result_output.split('"')[1]
+            console.log(Result_output)
+
         } else {
             res.send('generate report error.')
         }
@@ -21,6 +30,12 @@ app.get('/report', (req, res) => {
     process.on('error', (error) => console.log(error))
 })
 
+
+
+
+app.get('/download-file', function (req, res) {
+    res.download("./"+ idx);    
+});
 
 app.listen(8080, () => {
     console.log('server running on port 8080')
